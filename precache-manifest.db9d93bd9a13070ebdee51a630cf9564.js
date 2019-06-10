173b1e78bb6c39f092ac4845eafc94f7000537b5
Leandro Spagnoli <leaspag@hotmail.com>
2019-06-09 21:21:29 -0300

    Updates

---
+++ precache-manifest.db9d93bd9a13070ebdee51a630cf9564.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a86ebc0f5e1033ab30e32016311e9d3",
+    "url": "/strider-task-manager/index.html"
+  },
+  {
+    "revision": "01f8610073f917b2d7b2",
+    "url": "/strider-task-manager/static/css/main.19e2ed2c.chunk.css"
+  },
+  {
+    "revision": "22e275892326126c8875",
+    "url": "/strider-task-manager/static/js/2.ecbd5526.chunk.js"
+  },
+  {
+    "revision": "01f8610073f917b2d7b2",
+    "url": "/strider-task-manager/static/js/main.d0a0e36d.chunk.js"
+  },
+  {
+    "revision": "38104377bccc7f6b73bd",
+    "url": "/strider-task-manager/static/js/runtime~main.0489809b.js"
+  },
+  {
+    "revision": "d7fdebecd2b8f3a47137f97bd470df70",
+    "url": "/strider-task-manager/static/media/background.d7fdebec.jpg"
+  },
+  {
+    "revision": "19740d6007e61f13df3aedbebb93d304",
+    "url": "/strider-task-manager/static/media/logo.19740d60.svg"
+  },
+  {
+    "revision": "8dfc684e47bb6439b736efe486c783b9",
+    "url": "/strider-task-manager/static/media/placeholder.8dfc684e.png"
+  }
+]);
